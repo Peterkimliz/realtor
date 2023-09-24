3110d3546773e26df8c97bb6d5bbb936f2b5344e
PeterLojiKim <pkironji@yahoo.com>
2023-09-24 14:12:07 +0300

    configured the Websecurity class

---
+++ src/main/java/com/example/digirealtor/Security/WebSecurity.java
@@ -0,0 +1,66 @@
+package com.example.digirealtor.Security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity {
+
+    @Autowired
+    private HandlerExceptionResolver handlerExceptionResolver;
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    @Autowired
+    UserDetailsImplementation userDetailsImplementation;
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter(){
+        return new JwtAuthenticationFilter(handlerExceptionResolver);
+
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+
+        return
+                httpSecurity
+                        .csrf(csrf -> csrf
+                                .disable())
+                        .authorizeHttpRequests()
+                        .requestMatchers("/api/auth/**")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+                        .and()
+                        .sessionManagement(management -> management
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                        .authenticationProvider(authenticationProvider())
+                        .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+
+                        .build();
+    }
+
+
+@Bean
+public AuthenticationProvider authenticationProvider(){
+    DaoAuthenticationProvider daoAuthenticationProvider=new DaoAuthenticationProvider();
+    daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
+    daoAuthenticationProvider.setUserDetailsService(userDetailsImplementation);
+    return daoAuthenticationProvider;
+
+}
+
+  
+    
+}
