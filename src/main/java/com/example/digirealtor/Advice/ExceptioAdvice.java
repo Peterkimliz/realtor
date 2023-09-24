e893994a0c4833e5db4b342cbb5cf9fb627516ac
PeterLojiKim <pkironji@yahoo.com>
2023-09-24 16:12:02 +0300

    configured mongo uri

--- src/main/java/com/example/digirealtor/Advice/ExceptioAdvice.java
+++ src/main/java/com/example/digirealtor/Advice/ExceptioAdvice.java
@@ -2,7 +2,6 @@ package com.example.digirealtor.Advice;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.example.digirealtor.Exceptions.ExceptionObject;
