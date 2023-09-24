f7a206f16b3fa047259dce84a0633f8637a932e5
PeterLojiKim <pkironji@yahoo.com>
2023-09-24 12:14:22 +0300

    first commit

---
+++ src/test/java/com/example/digirealtor/DigirealtorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.digirealtor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigirealtorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
