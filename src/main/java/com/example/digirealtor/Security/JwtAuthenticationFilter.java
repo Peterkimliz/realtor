5d7b4918075f0e22dd82426dbceb3594c980cbd8
PeterLojiKim <pkironji@yahoo.com>
2023-09-24 13:43:11 +0300

    handled the jwt security exception at jwtAuthenticationFilter

--- src/main/java/com/example/digirealtor/Security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/digirealtor/Security/JwtAuthenticationFilter.java
@@ -7,20 +7,25 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@Component
+
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Autowired
     private JwtService jwtService;
     @Autowired 
     private UserDetailsImplementation userDetailsImple;
+    private HandlerExceptionResolver handlerExceptionResolver;
+
+    public JwtAuthenticationFilter (HandlerExceptionResolver handlerException){
+        this.handlerExceptionResolver=handlerException;
+    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -54,6 +59,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             filterChain.doFilter(request, response);
 
         } catch (Exception e) {
+            handlerExceptionResolver.resolveException(request, response, null, e);
             System.out.println(e);
         }
 
