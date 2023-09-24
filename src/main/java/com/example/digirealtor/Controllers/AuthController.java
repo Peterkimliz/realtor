e408fd8d04321e9e4053857e27a05933cddf5d8e
PeterLojiKim <pkironji@yahoo.com>
2023-09-24 15:58:10 +0300

    created the auth controller

---
+++ src/main/java/com/example/digirealtor/Controllers/AuthController.java
@@ -0,0 +1,34 @@
+package com.example.digirealtor.Controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.digirealtor.Dtos.LoginResponse;
+import com.example.digirealtor.Dtos.SigninRequests;
+import com.example.digirealtor.Services.AuthService;
+
+@RestController
+@RequestMapping("api/v1/auth")
+public class AuthController {
+
+    @Autowired
+    AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<LoginResponse> signUpUser(@RequestBody @Validated SigninRequests  signinRequests){
+        return new ResponseEntity<LoginResponse>(authService.createUser(signinRequests),HttpStatus.CREATED);
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<LoginResponse> signInUser(@RequestBody SigninRequests signinRequests){
+        return new ResponseEntity<LoginResponse>(authService.createUser(signinRequests),HttpStatus.CREATED);
+    }
+
+    
+}
