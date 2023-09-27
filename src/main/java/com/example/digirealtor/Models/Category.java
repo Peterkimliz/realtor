8701c6a2b8b8270017181e1fb8bac1059d018b3e
PeterLojiKim <pkironji@yahoo.com>
2023-09-27 21:23:24 +0300

    added the category model

---
+++ src/main/java/com/example/digirealtor/Models/Category.java
@@ -0,0 +1,25 @@
+package com.example.digirealtor.Models;
+
+import java.sql.Date;
+
+import org.hibernate.validator.constraints.UniqueElements;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document(collection = "categories")
+
+public class Category {
+    @Id
+    private String id;
+    @UniqueElements
+    private String name;
+    private Date createdAT;
+
+    
+}
